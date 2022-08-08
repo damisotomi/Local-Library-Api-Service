fbcd3e1bcb176b79aac283790b9f4f703aae4148
damisotomi <sotomidamilola@gmail.com>
2022-08-08 10:46:03 +0100

    Adjusting the db connection

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -87,7 +87,10 @@ DATABASES = {
         'PASSWORD':'sotomisql'
     }
 }
+import dj_database_url
 
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
