93c9a3d85d592e74be43e7e562e27f59bc381237
damisotomi <sotomidamilola@gmail.com>
2022-08-09 20:47:01 +0100

    added the data used to populate the postgres database

---
+++ data/catalog_book.sql
@@ -0,0 +1,106 @@
+insert into catalog_book (id, title, author_id, summary, isbn, language_id) values (1, 'Strategic Air Command', 30, 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.
+
+Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.
+
+In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.', '200043724-9', 2);
+insert into catalog_book (id, title, author_id, summary, isbn, language_id) values (2, 'Dear John', 17, 'Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.
+
+Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.
+
+Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.', '585908286-X', 6);
+insert into catalog_book (id, title, author_id, summary, isbn, language_id) values (3, 'Bad Country', 29, 'In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.', '481916186-5', 8);
+insert into catalog_book (id, title, author_id, summary, isbn, language_id) values (4, 'Saint Laurent', 12, 'Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis.
+
+Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.', '057645977-1', null);
+insert into catalog_book (id, title, author_id, summary, isbn, language_id) values (5, 'Before Midnight', 6, 'Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.', '400047188-0', 2);
+insert into catalog_book (id, title, author_id, summary, isbn, language_id) values (6, 'Cincinnati Kid, The', 8, 'In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.
+
+Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '070141713-7', 8);
+insert into catalog_book (id, title, author_id, summary, isbn, language_id) values (7, 'Metro', 3, 'In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.', '991235692-6', null);
+insert into catalog_book (id, title, author_id, summary, isbn, language_id) values (8, 'Invasion U.S.A.', 3, 'Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.
+
+Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.
+
+Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.', '589069385-9', 10);
+insert into catalog_book (id, title, author_id, summary, isbn, language_id) values (9, 'The 21 Carat Snatch', 14, 'Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.', '617993743-5', null);
+insert into catalog_book (id, title, author_id, summary, isbn, language_id) values (10, 'Indian in the Cupboard, The', 4, 'Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis.
+
+Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.
+
+Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.', '731582708-0', 7);
+insert into catalog_book (id, title, author_id, summary, isbn, language_id) values (11, 'At Five in the Afternoon (Panj é asr)', 1, 'In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.
+
+Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.
+
+Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.', '046201322-7', 2);
+insert into catalog_book (id, title, author_id, summary, isbn, language_id) values (12, 'Wes Craven''s New Nightmare (Nightmare on Elm Street Part 7: Freddy''s Finale, A)', 27, 'Quisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros.
+
+Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.
+
+In congue. Etiam justo. Etiam pretium iaculis justo.', '081998329-2', null);
+insert into catalog_book (id, title, author_id, summary, isbn, language_id) values (13, 'Earth Trembles, The (Terra trema: Episodio del mare, La)', 8, 'Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+
+Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.', '452893046-3', 6);
+insert into catalog_book (id, title, author_id, summary, isbn, language_id) values (14, 'Business, The', 27, 'Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.
+
+Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.', '209661366-3', 4);
+insert into catalog_book (id, title, author_id, summary, isbn, language_id) values (15, 'Hellraiser: Inferno', 22, 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin risus. Praesent lectus.', '361106659-1', 1);
+insert into catalog_book (id, title, author_id, summary, isbn, language_id) values (16, 'Common Wealth (Comunidad, La)', 12, 'Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.
+
+Phasellus in felis. Donec semper sapien a libero. Nam dui.
+
+Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.', '566928099-7', null);
+insert into catalog_book (id, title, author_id, summary, isbn, language_id) values (17, 'Dave Chappelle: For What it''s Worth', 23, 'Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.
+
+In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.
+
+Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.', '436464754-3', null);
+insert into catalog_book (id, title, author_id, summary, isbn, language_id) values (18, 'The Hearse', 10, 'Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.
+
+Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.
+
+Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.', '027684803-9', 6);
+insert into catalog_book (id, title, author_id, summary, isbn, language_id) values (19, 'About Alex', 6, 'Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.
+
+Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.', '288786771-7', 7);
+insert into catalog_book (id, title, author_id, summary, isbn, language_id) values (20, 'Next Day Air', 15, 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+
+Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.
+
+Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.', '391570572-1', 6);
+insert into catalog_book (id, title, author_id, summary, isbn, language_id) values (21, 'U.S. Seals', 22, 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.
+
+Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.', '400713801-X', 5);
+insert into catalog_book (id, title, author_id, summary, isbn, language_id) values (22, 'Emperor of the North (Emperor of the North Pole)', 4, 'Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.
+
+Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.', '930321008-5', null);
+insert into catalog_book (id, title, author_id, summary, isbn, language_id) values (23, 'Gorky Park', 30, 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.
+
+Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.
+
+In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.', '836000742-X', 2);
+insert into catalog_book (id, title, author_id, summary, isbn, language_id) values (24, 'Everly', 6, 'In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.', '334163410-X', null);
+insert into catalog_book (id, title, author_id, summary, isbn, language_id) values (25, 'America the Beautiful 2: The Thin Commandments ', 29, 'Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus.
+
+Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.
+
+Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', '441451753-2', 4);
+insert into catalog_book (id, title, author_id, summary, isbn, language_id) values (26, 'Danny Deckchair', 8, 'Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.
+
+Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.', '248432001-9', 6);
+insert into catalog_book (id, title, author_id, summary, isbn, language_id) values (27, 'Portrait of Maria (María Candelaria (Xochimilco))', 17, 'Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.
+
+Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus.', '570043755-4', 4);
+insert into catalog_book (id, title, author_id, summary, isbn, language_id) values (28, 'Corn Is Green, The', 12, 'Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.
+
+Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.
+
+Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.', '598097670-1', 4);
+insert into catalog_book (id, title, author_id, summary, isbn, language_id) values (29, 'Hotel', 1, 'Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.
+
+Aenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.
+
+Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.', '340392723-7', null);
+insert into catalog_book (id, title, author_id, summary, isbn, language_id) values (30, 'Look Who''s Talking Now', 3, 'Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.
+
+Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', '527028001-9', null);
---
+++ data/catalog_language.sql
@@ -0,0 +1,10 @@
+insert into catalog_language (name) values ('Dari');
+insert into catalog_language (name) values ('Chinese');
+insert into catalog_language (name) values ('Papiamento');
+insert into catalog_language (name) values ('Catalan');
+insert into catalog_language (name) values ('Quechua');
+insert into catalog_language (name) values ('Haitian Creole');
+insert into catalog_language (name) values ('Fijian');
+insert into catalog_language (name) values ('Guaraní');
+insert into catalog_language (name) values ('Arabic');
+insert into catalog_language (name) values ('Dutch');
